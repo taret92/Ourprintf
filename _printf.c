91b2fa8c5ccb494aad97db5c729c7fac25a76388
taret92 <ivancho92.10@hotmail.com>
2022-03-21 18:04:52 -0500

    se incluye a main.h el prototipo de _printf

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - funcion para imprimir parametros
+ * @format: formato a imprimir
+ * @i: primer iterador
+ * @return int 
+ */
+
+
+int _printf(const char *format, ...);
+
+va_list list;
+	int i, len = 0;
+	int (*f)(va_list);
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			len++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+		{
+			return (-1);
+		}
+		f = get_commands(format + i + 1);
+		if (f != NULL)
+		{
+			len = len + f(list);
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;/*igual al numero de veces que se imprime*/
+		}
+	}
+	va_end(list);
+	return (len);/*retorna el numero de veces que se imprime*/
