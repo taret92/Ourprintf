e55dd7ede8cb8858b11d4bc0f8c77a1ca1083f39
taret92 <ivancho92.10@hotmail.com>
2022-03-22 20:41:47 -0500

    Fix _printf

--- _printf.c
+++ _printf.c
@@ -1,26 +1,23 @@
 #include "main.h"
-
 /**
  * _printf - funcion para imprimir parametros
  * @format: formato a imprimir
- * @i: iterador
- * @return int
+ * Return: int
  */
-
-
-int _printf(const char *format, ...);
-
+int _printf(const char *format, ...)
+{
 va_list list;
-	int i, len = 0;
+	int i = 0, len = 0; /**inicializar variables*/
 	int (*f)(va_list);
-	
+
+	while (format[i]) /**condicion para iterar*/
+	{
+	for (; format[i] != '%' && format[i]; i++)
 	if (format == NULL) /*si es NULL retorna error*/
 	{
 		return (-1);
 	}
-
 	va_start(list, format);
-
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] != '%')
@@ -30,9 +27,7 @@ va_list list;
 			continue;
 		}
 		if (format[i + 1] == '\0')
-		{
 			return (-1);
-		}
 		f = get_format(format + i + 1);
 		if (f != NULL)
 		{
@@ -42,8 +37,10 @@ va_list list;
 		else
 		{
 			_putchar(format[i]);
-			len++;//igual al numero de veces que se imprime
+			len++;	/**igual al numero de veces que se imprime*/
 		}
 	}
+	}
 	va_end(list);
-	return (len);//retorna el numero de veces que se imprime
+	return (len);/**retorna lo que se va a imprimir*/
+}
