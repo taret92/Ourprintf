01ea0d15e657e465b7d5963e0ac1ff1804a9f663
xARKEINx <3884@holbertonschool.com>
2022-03-22 19:59:34 -0700

    solo%

---
+++ print_pc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_pc - print %
+ * @list: variable
+ * Return: 1
+ */
+int print_pc(va_list __attribute__((unused)) list)/*añade un caracter*/
+{
+	_putchar ('%');
+	return (1);
+}
