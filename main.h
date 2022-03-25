41b2c36278f3defde95b601904fdce28c726ea99
taret92 <ivancho92.10@hotmail.com>
2022-03-24 23:08:26 -0500

    Corrección printstring

--- main.h
+++ main.h
@@ -26,10 +26,5 @@ int print_string(va_list list);
 int print_int(va_list list);
 int print_dex(va_list);
 int print_pc(va_list __attribute__((unused)) list);
-<<<<<<< HEAD
-int (*get_format(const char *s))(va_list);
-#endif
-=======
 int (*get_commands(const char *s))(va_list);
 #endif
->>>>>>> 573576ac13e400f921c20f49a1aa9138feed9f8c
